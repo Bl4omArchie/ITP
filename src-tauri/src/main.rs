64a61de6e8fcecbecd66c5744968b293fccd851d
Bl4omArchie <grlch.alexandre@proton.me>
2025-01-07 15:51:34 +0100

    snort boxes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,28 +1,52 @@
-use tauri::Window;
+use tauri::{
+    menu::{AboutMetadata, MenuBuilder, MenuItemBuilder, SubmenuBuilder},
+    Manager,
+};
+use tauri::Emitter;
 
-#[tauri::command]
-fn minimize(window: Window) {
-    window.minimize().unwrap();
-}
-
-#[tauri::command]
-fn maximize(window: Window) {
-    window.maximize().unwrap();
-}
-
-#[tauri::command]
-fn unmaximize(window: Window) {
-    window.unmaximize().unwrap();
-}
-
-#[tauri::command]
-fn close(window: Window) {
-    window.close().unwrap();
-}
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![minimize, maximize, unmaximize, close])
+        .setup(|app| {
+            let handle = app.handle();
+            
+            // my custom settings menu item
+            let settings = MenuItemBuilder::new("Settings...")
+                .id("settings")
+                .accelerator("CmdOrCtrl+,")
+                .build(app)?;
+
+            // my custom app submenu
+            let app_submenu = SubmenuBuilder::new(app, "App")
+                .about(Some(AboutMetadata {
+                    ..Default::default()
+                }))
+                .separator()
+                .item(&settings)
+                .separator()
+                .services()
+                .separator()
+                .hide()
+                .hide_others()
+                .quit()
+                .build()?;
+
+            let menu = MenuBuilder::new(app)
+                .items(&[
+                    &app_submenu,
+                ])
+                .build()?;
+
+            app.set_menu(menu)?;
+            app.on_menu_event(move |app, event| {
+                if event.id() == settings.id() {
+                    // emit a window event to the frontend 
+                    let _event = app.emit("custom-event", "/settings");
+                }
+            });
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/pages/Homepage.vue
+++ src/components/pages/Homepage.vue
@@ -1,10 +1,58 @@
 <template>
-    <h1>Homepage</h1>
+  <div>
+      <h1>Homepage</h1>
+      <div id="traffic-graph"></div>
+      
+      <div class="snortBox snortBox--horizontal">
+        <p>My first box</p>
+      </div>
+
+      <div class="snortBox snortBox--vertical">
+          <p>my second box</p>
+      </div>
+  </div>
 </template>
 
 <script lang="ts">
-    
+    import { onMounted } from 'vue';
+    import * as d3 from 'd3';
+    import "../../styles/graphics/line_plot.css";
+    import "../../styles/boxes/snort_box.css";
+
     export default {
       name: 'Home',
+      setup() {
+        onMounted(() => {
+          const data = [10, 20, 30, 40, 30, 50, 60, 70, 40, 50, 30, 80, 90, 70, 10, 20, 30, 25, 30, 41]; // Sample network traffic data
+
+          const width = 600;
+          const height = 400;
+          
+          const svg = d3.select('#traffic-graph')
+                        .append('svg')
+                        .attr('width', width)
+                        .attr('height', height);
+
+          const xScale = d3.scaleLinear()
+                          .domain([0, data.length - 1]) // Data length range
+                          .range([0, width]); // Pixel width
+
+          const yScale = d3.scaleLinear()
+                          .domain([0, d3.max(data) || 0]) // Y-axis range
+                          .range([height, 0]); // Pixel height (inverted)
+
+          const line = d3.line<number>()
+                        .x((d, i) => xScale(i))
+                        .y(d => yScale(d));
+
+          svg.append('path')
+            .data([data])
+            .attr('class', 'line')
+            .attr('d', line)
+            .attr('fill', 'none')
+            .attr('stroke', 'blue')
+            .attr('stroke-width', 2);
+        });
+      }
     };
 </script>
