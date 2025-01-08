cd781837970e7188bd4b008af6e4805e913ed12d
Bl4omArchie <grlch.alexandre@proton.me>
2025-01-08 15:04:03 +0100

    fix popup

--- src/components/pages/Modules.vue
+++ src/components/pages/Modules.vue
@@ -1,34 +1,33 @@
 <template>
-    <div class="main-body">
-        <h1>Modules</h1>
-        <button class="snortButton snortButton--small" @click="showPopup = true">Open Popup</button>
-    
+    <h1>Modules</h1>
+    <button class="snortButton snortButton--small" @click="showPopup = true">Open Popup</button>
 
-        <div v-if="showPopup" class="snortPopup-overlay" @click.self="closePopup">
-            <div class="snortPopup">
-                <button class="snortPopup-close" @click="closePopup">&times;</button>
-                
-                <div class="snortPopup-content">
-                    <h2>Popup Title</h2>
-                    <p>Hello World!</p>
-                </div>
-            </div>
-        </div>
+    <div v-if="showPopup" class="snortPopup-overlay" @click.self="closePopup">
+      <div class="snortPopup">
+          <button class="snortPopup-close" @click="closePopup">&times;</button>
+          
+          <div class="snortPopup-content">
+              <h2>Popup Title</h2>
+              <p>Hello World!</p>
+          </div>
+      </div>
     </div>
-  </template>
+</template>
+
   
 <script lang="ts">
-  export default {
-    name: 'Modules',
-    data() {
-      return {
-        showPopup: false,
-      };
-    },
-    methods: {
-      closePopup() {
-        this.showPopup = false;
+    import "../../styles/boxes/snort_popup.css";
+    export default {
+      name: 'Modules',
+      data() {
+        return {
+          showPopup: false,
+        };
       },
-    },
-  };
+      methods: {
+        closePopup() {
+          this.showPopup = false;
+        },
+      },
+    };
 </script>
