697c30b58e2296db5f098abf08fb3ddf8d6bdb81
Bl4omArchie <grlch.alexandre@proton.me>
2025-01-09 17:58:53 +0100

    add store

--- src/components/pages/Modules.vue
+++ src/components/pages/Modules.vue
@@ -1,33 +1,45 @@
 <template>
     <h1>Modules</h1>
-    <button class="snortButton snortButton--small" @click="showPopup = true">Open Popup</button>
+    
+    <div class="snortBox">
+        <h2>All Tools</h2>
+        <div v-for="(tool, name) in tools" :key="name" class="snortBox--h">
 
-    <div v-if="showPopup" class="snortPopup-overlay" @click.self="closePopup">
-      <div class="snortPopup">
-          <button class="snortPopup-close" @click="closePopup">&times;</button>
-          
-          <div class="snortPopup-content">
-              <h2>Popup Title</h2>
-              <p>Hello World!</p>
-          </div>
-      </div>
+            <div class="snortBox__logo">
+              <img :src="tool.logo_url || defaultLogo" :alt="`${name} logo`" class="snortBox__logo-img"/>
+            </div>
+
+            <p>Name: {{ name }}</p>
+            <p>Git: <a :href="tool.git" target="_blank">Lien</a></p>
+            <p>Version: {{ tool.version }}</p>
+        </div>
     </div>
 </template>
 
-  
 <script lang="ts">
-    import "../../styles/boxes/snort_popup.css";
-    export default {
-      name: 'Modules',
-      data() {
-        return {
-          showPopup: false,
-        };
-      },
-      methods: {
-        closePopup() {
-          this.showPopup = false;
+    import { defineComponent, ref, onMounted } from "vue";
+    import "../../styles/boxes/snort_box.css";
+
+    interface Tool {
+        git: string;
+        version: string;
+        package_name: string;
+        logo_url: string | null;
+    }
+
+    export default defineComponent({
+        name: "SnortBox",
+        setup() {
+            const tools = ref<Record<string, Tool>>({});
+            const defaultLogo = "https://vectorified.com/images/default-icon-16.png";
+
+            onMounted(async () => {
+                const response = await fetch("/tools.json");
+                tools.value = await response.json();
+            });
+
+            return { tools, defaultLogo };
         },
-      },
-    };
+    });
+
 </script>
--- src/components/pages/Rules.vue
+++ src/components/pages/Rules.vue
@@ -1,12 +1,33 @@
 <template>
-    <div class="main-body">
-        <h1>Rules</h1>
+    <h1>Rules</h1>
+    <button class="snortButton snortButton--small" @click="showPopup = true">Open Popup</button>
+
+    <div v-if="showPopup" class="snortPopup-overlay" @click.self="closePopup">
+      <div class="snortPopup">
+          <button class="snortPopup-close" @click="closePopup">&times;</button>
+          
+          <div class="snortPopup-content">
+              <h2>Popup Title</h2>
+              <p>Hello World!</p>
+          </div>
+      </div>
     </div>
 </template>
 
+  
 <script lang="ts">
-    import "../../styles/Pages.css";
+    import "../../styles/boxes/snort_popup.css";
     export default {
       name: 'Rules',
+      data() {
+        return {
+          showPopup: false,
+        };
+      },
+      methods: {
+        closePopup() {
+          this.showPopup = false;
+        },
+      },
     };
 </script>
